ee6c225040c9ee637a57a3dd814ba874e16e7ac0
maxstarvaggi <starvaggi.7@osu.edu>
2021-12-01 16:37:35 -0500

    Removed redundant email validation message

--- PeerEvaluationTool/app/models/user.rb
+++ PeerEvaluationTool/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 50 }
 
   EMAIL_FORMAT = URI::MailTo::EMAIL_REGEXP
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, length: { maximum: 255 },
                     format: { with: EMAIL_FORMAT },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true
