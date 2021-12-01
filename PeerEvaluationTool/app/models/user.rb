84d58bea958da1364c6da5925f1ca708b6960dbb
maxstarvaggi <starvaggi.7@osu.edu>
2021-12-01 16:36:05 -0500

    Signup input validation working

--- PeerEvaluationTool/app/models/user.rb
+++ PeerEvaluationTool/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :email, :first_name, :last_name, :password
   before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
@@ -7,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_FORMAT },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+
 end
--- PeerEvaluationTool/db/migrate/20211201182321_add_password_hash_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddPasswordHashToUsers < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
