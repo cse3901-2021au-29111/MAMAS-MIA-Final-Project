bb35f4a39b2b8c2cdbb91f63a701cfb85978cf3a
maxstarvaggi <starvaggi.7@osu.edu>
2021-12-02 11:20:48 -0500

    Added login page & path

--- PeerEvaluationTool/config/routes.rb
+++ PeerEvaluationTool/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # Home page root route
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
